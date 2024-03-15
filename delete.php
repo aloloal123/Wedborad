3c6be3b7d38c8c6e10d52f624f565d19e27f25c2
aloloal123 <aloloal409@gmail.com>
2024-03-15 12:10:33 +0700

    work10 js
    work10 js

--- delete.php
+++ delete.php
@@ -1,8 +1,15 @@
 <?php 
 session_start();
-$p = $_GET['id'];
 if(isset($_SESSION['id']) && $_SESSION['role'] == 'a'){
-        echo "ลบกระทู้ หมายเลข $p";
+        $id=$_GET['id'];
+        $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
+        $sql="DELETE FROM post WHERE id=$id";
+        $conn->exec($sql);
+        $sql="DELETE FROM comment WHERE post_id=$id";
+        $conn->exec($sql);
+        $conn = null;
+        header("Location:index.php");
+        die();
  }
 
 else
--- post.php
+++ post.php
@@ -1,9 +1,6 @@
 <?php
 session_start();
-if (!isset($_SESSION['id'])) {
-    header("Location:index.php");
-    die();
-} ?>
+?>
 <!DOCTYPE html>
 <html lang="en">
 
@@ -21,43 +18,59 @@ if (!isset($_SESSION['id'])) {
         <h1 style="text-align: center;" class="mt-3">Webboard kakkak</h1>
         <?php include "nav.php" ?>
         <div class="row mt-4">
-        <div class="col-lg-3 col-md-2 col-sm-1"></div>
-        <div class="col-lg-6 col-md-8 col-sm-10">
-<?php
-    $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
-    $sql="SELECT post.title, post.content, post.post_date ,user.login FROM post INNER JOIN user ON (post.user_id=user.id) WHERE post.id=$_GET[id]";
-    $result=$conn->query($sql);
-    while($row=$result->fetch()){
-       echo "<div class='card border-primary mt-3'>
+            <div class="col-lg-3 col-md-2 col-sm-1"></div>
+            <div class="col-lg-6 col-md-8 col-sm-10">
+                <?php
+                $conn = new PDO("mysql:host=localhost;dbname=webboard;charset=utf8", "root", "");
+                $sql = "SELECT post.title, post.content, post.post_date ,user.login FROM post INNER JOIN user ON (post.user_id=user.id) WHERE post.id=$_GET[id]";
+                $result = $conn->query($sql);
+                while ($row = $result->fetch()) {
+                    echo "<div class='card border-primary mt-3'>
                 <div class='card-header bg-primary text-white'>$row[0]</div>
                 <div class='card-body'>$row[1]<br><br>$row[3] - $row[2] 
-                </div></div>"   ; }
-?>
+                </div></div>";
+                }
 
-            <div class="card border-success mt-3">
-                <div class="card-header bg-success text-white">แสดงความคิดเห็น</div>
-                <div class="card-body">
-                    <form action="post_save.php" method="post">
-                        <input type="hidden" name="post_id" value="<?= $_GET['id'] ?>">
+                $sql = "SELECT comment.content,comment.post_date,user.login
+                FROM comment INNER JOIN user ON 
+                (comment.user_id=user.id) WHERE comment.post_id=$_GET[id]";
+                $result = $conn->query($sql);
+                $num = 1;
+                while ($row = $result->fetch()) {
+                    echo "<div class='card border-info mt-3'>
+                <div class='card-header bg-info text-white'>ความคิดเห็น $num </div>
+                <div class='card-body'>$row[content]<br><br>$row[2] - $row[1] 
+                </div></div>";
+                $num++;
+                }
+                $conn = null;
+                ?>
+<?php if (isset($_SESSION['id'])) {
+ ?>
+                <div class="card border-success mt-3">
+                    <div class="card-header bg-success text-white">แสดงความคิดเห็น</div>
+                    <div class="card-body">
+                        <form action="post_save.php" method="post">
+                            <input type="hidden" name="post_id" value="<?= $_GET['id'] ?>">
 
-                        <div class="row mb-3 justify-content-center">
-                            <div class="col-lg-10">
-                                <textarea name="comment" rows="8" class="form-control" required></textarea>
+                            <div class="row mb-3 justify-content-center">
+                                <div class="col-lg-10">
+                                    <textarea name="comment" rows="8" class="form-control" required></textarea>
+                                </div>
                             </div>
-                        </div>
-                        <div class="row">
-                            <div class="col-lg-12 d-flex justify-content-center">
-                                <button type="submit" class="btn btn-success btn-sm text-white">
-                                    <i class="bi bi-box-arrow-up-right"></i> ส่งข้อความ
-                                </button>
-                                <button type="reset" class="btn btn-danger btn-s ms-2"><i class="bi bi-x-square"></i> ยกเลิก</button>
+                            <div class="row">
+                                <div class="col-lg-12 d-flex justify-content-center">
+                                    <button type="submit" class="btn btn-success btn-sm text-white">
+                                        <i class="bi bi-box-arrow-up-right"></i> ส่งข้อความ
+                                    </button>
+                                    <button type="reset" class="btn btn-danger btn-s ms-2"><i class="bi bi-x-square"></i> ยกเลิก</button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
-
+                        </form>
+                    </div>
                 </div>
-            </div>
-            <br>
+            <?php } ?>
+                <br>
             </div>
             <div class="col-lg-3 col-md-20 col-sm-1"></div>
         </div>
@@ -66,5 +79,3 @@ if (!isset($_SESSION['id'])) {
 </body>
 
 </html>
-
-
